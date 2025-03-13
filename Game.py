2e8fbb8a2468d81602d6e99081b145a532bfe6a5
Sean Takahashi <seanyuto@gmail.com>
2025-03-12 21:29:04 -0400

    added assignment python file

---
+++ Game.py
@@ -0,0 +1,41 @@
+## Lab05- TicTacToe ##
+
+# This program is a simple TicTacToe game for the user to play the computer(random choice).
+# You will create a n by n tic-tac-toe game where the user decides the size of the board.
+
+
+# Create a n*n board with all empty spaces
+def createBoard(n):
+    return None # replace this line along with your code
+
+
+# Asks the user for the row and column to place their move
+def userInput():
+    return None # replace this line along with your code
+
+
+# Randomly selects a row and column to place the computer's move, has to be open slot
+def computerChoose(board):
+    return None # replace this line along with your code
+
+
+# Checks if either player won(n in a row vertical, horizontal, or diagonal)
+def checkWin(board):
+    return None # replace this line along with your code
+
+
+# Checks if draw(board is full)
+def isBoardFull(board):
+    return None # replace this line along with your code
+
+
+# Prints the board
+def printBoard(board):
+    return None # replace this line along with your code
+
+
+# Wrapper function(prints board after each turn, lets user repeatedly play after game ends, keeps track of score)
+# You should ask user input if they would like to continue playing after each game
+# If the user inputs "quit" at any point, the program should stop
+def main():
+    return None # replace this line along with your code
